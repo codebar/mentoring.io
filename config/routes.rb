81a32ddc0f4e20aea92aa367fe5b1b408c8e6da6
Dorothy Wingrove <dorothy@obdev.co.uk>
2014-10-18 20:35:52 +0100

    oops fix new mentor route

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :members, only: [:index, :new, :create] do
-    get :new_mentor
+    # get :new_mentor
     get :profile_form
     put :create_profile
     resources :member_skills, only: [:create, :new]
     resources :classified, only: [:create, :new]
   end
 
+  get 'new_mentor', to: 'members#new_mentor'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
