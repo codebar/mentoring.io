2cf2f9a974e967d40b02c6a1e0888c8298075f42
Dorothy Wingrove <dorothy@obdev.co.uk>
2014-10-18 15:11:25 +0100

    expertise accessor members model

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,16 @@
 class Member < ActiveRecord::Base
+
+  cattr_reader :expertise
+  @@expertise = {
+    1 => 'No programming knowledge',
+    2 => 'Im familiar with html and css',
+    3 => 'I know some basic programming',
+    4 => 'Junior developer',
+    5 => 'Mid-level developer',
+    6 => 'Senior developer'
+  }
+
+  def validate
+    validates_presence [:username, :email, :full_name]
+  end
 end
