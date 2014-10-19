539310fb191826e73cd4a730d4449d66afb367cb
Dorothy Jane Wingrove <wingrove.d@gmail.com>
2014-10-19 23:03:50 +0100

    slight classified search improvements

--- app/models/classified.rb
+++ app/models/classified.rb
@@ -9,20 +9,20 @@ class Classified < ActiveRecord::Base
 
   def self.search(params)
     search_clause = []
-    if params[:location]
+    if !params[:location].blank?
       search_clause << "members.location like '%#{params[:location]}%'"
     end
     if params[:remote]
       search_clause << "remote is 'true'"
     end
     if params[:face_to_Face]
-      search_clause << "face_to_face is #{params[:face_to_face]}"
+      search_clause << "face_to_face is 'true'"
     end
     if params[:skill_ids]
       search_clause << "skills.id in ('#{params[:skill_ids].join("','")}')"
     end
     search_clause = search_clause.join(" and ").to_s
     puts "search_clause : #{search_clause}"
-    Classified.joins(:member).joins(:skills).where(search_clause)
+    Classified.select("classifieds.*").joins(:member).joins(:skills).where(search_clause).group("classifieds.id")
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20141019205937) do
     t.boolean  "mentor"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "location"
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -77,7 +78,6 @@ ActiveRecord::Schema.define(version: 20141019205937) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.string   "location"
     t.string   "provider"
     t.string   "uid"
   end
