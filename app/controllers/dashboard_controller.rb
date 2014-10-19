adc0761ddeb26ab20237645e3a7c65837329913e
Rosa Fox <rosafox89@gmail.com>
2014-10-19 17:11:28 +0100

    Added links to the dashboard, styling still needs some work

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,11 @@
 class DashboardController < ApplicationController
-
+  before_filter :set_member, only: [:index]
   def index
+  end
+
+  private
+
+  def set_member
+    @member = current_member
   end
 end
