6ae0e3e3ae73fec34c856346964cde32b1f97217
Despo Pentara <despo@extractmethod.com>
2014-10-19 02:10:24 +0100

    configure action mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,7 @@ Rails.application.configure do
     :authentication => :plain
   }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => ENV['HOST'] }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
